6762a1da308de72964764149488736582629d2cf
xclhove <xclhove@qq.com>
2025-05-04 00:42:42 +0800

    ci: 修改 docker build 脚本

--- docker-build.sh
+++ docker-build.sh
@@ -5,6 +5,11 @@ if [ ! -d "$build_dir" ]; then
   npm run build || yarn run build || (echo "构建失败" && exit 1)
 fi
 
-tag=ghcr.io/xclhove/xnote-nuxt:latest
+version=$1
+if [ -z "$version" ]; then
+    version=$(date +1.%y%m%d.%H%M%S)
+fi
+
+tag=ghcr.io/xclhove/xnote-nuxt:$version
 docker build . -t $tag
 echo $GH_PAT | docker push $tag
